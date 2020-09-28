f757b0ec24f437075a6d0a23afd1770475c49129
Null <66488397+NullByte75@users.noreply.github.com>
2020-09-28 15:19:39 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+pip3 install discord
+pip3 install asyncio
+pip3 install os
+pip3 install subprocess
+pip3 install time
+pip3 install opencv-python
+pip3 install time
+pip3 install pyautogui
+pip3 install tkinter
+pip3 install gtts
+pip3 install playsound
+pip3 install sounddevice
+pip3 install scipy
+pip3 install wavio
+python3 main.py
